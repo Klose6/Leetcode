c49285742373000aaf744cd0e6d37c7aa2e71a9e
Klose6 <scsehebut@gmail.com>
2018-05-02 23:55:33 -0700

    May2:1

--- 64_minimum_path_sum.py
+++ 64_minimum_path_sum.py
@@ -14,3 +14,16 @@ def min_path_sum(grid):
 			elif i and j:
 				grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
 	return grid[i - 1][j - 1]
+
+
+def min_path_sum2(grid):
+	m, n = len(grid), len(grid[0])
+	dp = [0] * n
+	dp[0] = grid[0][0]
+	for j in range(1, n):
+		dp[j] = dp[j - 1] + grid[0][j]
+	for i in range(1, m):
+		dp[0] += grid[i][0]
+		for j in range(1, n):
+			dp[j] = min(dp[j - 1], dp[j]) + grid[i][j]
+	return dp[n - 1]
