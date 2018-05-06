ec8066e1d596d615b2e5116534aea75c77b42e9c
Klose6 <scsehebut@gmail.com>
2018-05-06 12:03:36 -0700

    May6:1

--- 75_sort_colors.py
+++ 75_sort_colors.py
@@ -9,11 +9,11 @@ def sort_colors(nums):
 	n = len(nums)
 	lo, hi = 0, n - 1
 	i = 0
-	while i <= hi:
-		if nums[i] == 0:
+	while i < hi:
+		if i != lo and nums[i] == 0:
 			nums[lo], nums[i] = nums[i], nums[lo]
 			lo += 1
-		if nums[i] == 2:
+		if i != hi and nums[i] == 2:
 			nums[hi], nums[i] = nums[i], nums[hi]
 			hi -= 1
 		i += 1
