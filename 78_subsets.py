a061edadc5f92f0767d953698d8ef2f2ea289351
Klose6 <scsehebut@gmail.com>
2018-05-06 19:34:28 -0700

    May6:1

---
+++ 78_subsets.py
@@ -0,0 +1,28 @@
+"""
+subsets
+"""
+
+
+def subsets(nums):
+	if not nums:
+		return []
+	res = []
+	dfs(nums, 0, [], res)
+	return res
+
+
+def dfs(nums, start, cur, res):
+	res.append(cur)
+	for i in range(start, len(nums)):
+		dfs(nums, i + 1, cur + [nums[i]], res)
+
+
+def subsets2(nums):
+	res = [[]]
+	for num in nums:
+		res += [item + [num] for item in res]
+	return res
+
+
+print subsets([1, 2, 3])
+print subsets2([1, 2, 3])
