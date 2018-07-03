77bebe94927456add712ebf9b83105668c506dbc
Klose6 <scsehebut@gmail.com>
2018-07-02 22:20:48 -0700

    Jul7:1

---
+++ 863_all_nodes_distance_in_bt.py
@@ -0,0 +1,52 @@
+"""
+863 all node distance K in binary tree
+"""
+import collections
+
+
+class Node(object):
+	def __init__(self, val):
+		self.val = val
+		self.left, self.right = None, None
+
+
+class solution(object):
+	def distanceK(self, root, target, K):
+		def dfs(node, parent=None):
+			if node:
+				root.parent = parent
+				dfs(root.left, parent=root)
+				dfs(root.right, parent=root)
+
+		dfs(root)
+		queue = collections.deque([(target, 0)])
+		visited = [target]
+		while queue:
+			if queue[0][1] == K:
+				return [node.val for node, d in queue]
+			node, d = queue.popleft()
+			for nei in (node.parent, node.left, node.right):
+				if nei and nei not in visited:
+					visited.append(nei)
+					queue.append((nei, d + 1))
+		return []
+
+	def distanceK1(self, root, target, K):
+		conn = collections.defaultdict(list)
+
+		def connect(parent, child):
+			if parent and child:
+				conn[parent.val].append(child)
+				conn[child.val].append(parent)
+			if child.left:
+				connect(child, child.left)
+			if child.right:
+				connect(child, child.right)
+
+		connect(None, root)
+		bfs = [target]
+		seen = set(bfs)
+		for i in xrange(K):
+			bfs = [y for x in bfs for y in conn[x]]
+			seen |= set(bfs)
+		return bfs
