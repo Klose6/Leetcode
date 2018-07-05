4305f99243a0656806671eb6e313fe65443b7e4c
Klose6 <scsehebut@gmail.com>
2018-07-04 22:43:23 -0700

    Jul4:1

---
+++ 855_exam_room.py
@@ -0,0 +1,25 @@
+"""
+855 exam room
+"""
+
+
+class ExamRoom(object):
+	def __init__(self, N):
+		# L to record the seats that people sit
+		self.N, self.L = N, []
+
+	def seat(self):
+		N, L = self.N, self.L
+		if not L:
+			return 0
+		else:
+			d, res = L[0], 0
+			for a, b in zip(L, L[1:]):
+				if (b - a) / 2 > d:
+					d, res = (b - a) / 2, (b + a) / 2
+			if N - 1 - L[-1] > d:
+				res = N - 1
+		return res
+
+	def leave(self, p):
+		self.L.remove(p)
