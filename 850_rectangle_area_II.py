b0caac439d99a0b76d95a503bfb4f777cc0757f3
Klose6 <scsehebut@gmail.com>
2018-07-09 21:52:38 -0700

    Jul9:1

---
+++ 850_rectangle_area_II.py
@@ -0,0 +1,63 @@
+"""
+850 rectangle area II
+
+"""
+
+
+class Solution(object):
+	def rectangle_area(self, rectangles):
+		# using segment tree
+		OPEN, CLOSE = 1, -1
+		events = []
+		X = set()
+		for x1, y1, x2, y2 in rectangles:
+			events.append((y1, OPEN, x1, x2))
+			events.append((y2, CLOSE, x1, x2))
+			X.add(x1)
+			X.add(x2)
+		events.sort()
+		X = sorted(X)
+		Xi = {x: i for i, x in enumerate(X)}
+		st = SegmentTreeNode(0, len(X) - 1)
+		cur_y = events[0][0]
+		cur_x_sum = area = 0
+		for y, sign, x1, x2 in events:
+			area += (y - cur_y) * cur_x_sum
+			cur_x_sum = st.update(X, Xi[x1], Xi[x2], sign)
+			cur_y = y
+		return area % (10 ** 9 + 7)
+
+
+class SegmentTreeNode(object):
+	def __init__(self, start, end):
+		self.start, self.end = start, end
+		self.total = self.count = 0
+		self.left = self.right = None
+
+	def mid(self):
+		return (self.start + self.end) / 2
+
+	def left(self):
+		self.left = self.left or SegmentTreeNode(self.start, self.mid())
+		return self.left
+
+	def right(self):
+		self.right = self.right or SegmentTreeNode(self.mid(), self.end)
+		return self.right
+
+	def update(self, X, i, j, val):
+		if i >= j:
+			return 0
+		if self.start == i and self.end == j:
+			self.count += val
+		else:
+			self.left().update(X, i, min(self.mid(), j), val)
+			self.right().update(X, max(self.mid(), i), j, val)
+		if self.count > 0:
+			self.total = X[self.end] - X[self.start]
+		else:
+			self.total = self.left().total + self.right().total
+		return self.total
+
+
+print Solution().rectangle_area([[0, 0, 2, 2], [1, 0, 2, 3], [1, 0, 3, 1]])  # 6
