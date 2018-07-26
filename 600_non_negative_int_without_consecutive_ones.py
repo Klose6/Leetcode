f5ff6ef54bfc7986585df34224b87b275060c113
Klose6 <scsehebut@gmail.com>
2018-07-25 22:24:35 -0700

    Jul25:1

---
+++ 600_non_negative_int_without_consecutive_ones.py
@@ -0,0 +1,20 @@
+"""
+600 Non-negative integers without consecutive ones
+"""
+
+
+class Solution(object):
+	def findIntegers(self, num):
+		x, y = 1, 2
+		res = 0
+		num += 1
+		while num:
+			if num & 1 and num & 2:
+				res = 0
+			res += x * (num & 1)
+			num >>= 1
+			x, y = y, x + y
+		return res
+
+
+print Solution().findIntegers(5)
