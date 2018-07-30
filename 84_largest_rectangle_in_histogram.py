e25623eb43172bb5e3b229eff6144cd8a2c51584
Klose6 <scsehebut@gmail.com>
2018-07-29 22:38:41 -0700

    Jul29:1

--- 84_largest_rectangle_in_histogram.py
+++ 84_largest_rectangle_in_histogram.py
@@ -1,5 +1,13 @@
 """
 84 largest rectangle in histogram
+
+The stack maintain the indexes of buildings with ascending height.
+Before adding a new building pop the building who is taller than
+the new one. The building popped out represent the height of a
+rectangle with the new building as the right boundary and the
+current stack top as the left boundary. Calculate its area and
+update ans of maximum area. Boundary is handled using dummy
+buildings
 """
 
 
@@ -8,7 +16,7 @@ def largest_rectangle(heights):
 	stack = [-1]
 	heights.append(0)
 	for i in range(len(heights)):
-		while heights[i] < heights[stack[-1]]:
+		while stack and heights[i] < heights[stack[-1]]:
 			h = heights[stack.pop()]
 			w = i - stack[-1] - 1
 			res = max(res, h * w)
