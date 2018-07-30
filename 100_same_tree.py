a176050674dca9d2c19687abf18680d1623ebc8b
Klose6 <scsehebut@gmail.com>
2018-07-30 09:36:17 -0700

    Jul30:1

---
+++ 100_same_tree.py
@@ -0,0 +1,14 @@
+"""
+100 Same Tree
+"""
+
+
+class Solution(object):
+	def is_same(self, p, q):
+		if not p and not q:
+			return True
+		if not p or not q:
+			return False
+		if p.val == q.val:
+			return self.is_same(p.left, q.left) and self.is_same(p.right, q.right)
+		return False
