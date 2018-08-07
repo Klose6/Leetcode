f7091f48975cc262ef16d32b4f6af7ca43e4a932
Klose6 <scsehebut@gmail.com>
2018-08-06 21:42:25 -0700

    Aug6:1

--- 141_linked_list_cycles.py
+++ 141_linked_list_cycles.py
@@ -3,6 +3,11 @@
 """
 
 
+class Node(object):
+	def __init__(self, val):
+		self.val = val
+		self.next = None
+
 def has_cycle(head):
 	if not head:
 		return False
@@ -13,3 +18,10 @@ def has_cycle(head):
 		slow = slow.next
 		fast = fast.next.next
 	return False
+
+
+n1 = Node(1)
+n2 = Node(2)
+n1.next = n2
+# n2.next = n1
+print has_cycle(n1)
