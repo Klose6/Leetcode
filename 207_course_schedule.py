e0e4e0aa2fb407e35603293be329268a0e709347
Klose6 <scsehebut@gmail.com>
2018-08-12 23:12:40 -0700

    Aug12:1

--- 207_course_schedule.py
+++ 207_course_schedule.py
@@ -8,19 +8,22 @@ def can_finish(num_courses, prerequisites):
 	matrix = [[0] * num_courses for _ in range(num_courses)]
 	indegrees = [0] * num_courses
 	for pre in prerequisites:
-		if matrix[pre[0]][pre[1]] == 0:
+		if matrix[pre[1]][pre[0]] == 0:
 			indegrees[pre[0]] += 1  # duplicate case
-		matrix[pre[0]][pre[1]] = 1
+		matrix[pre[1]][pre[0]] = 1
 	q, count = [], 0
 	for i, v in enumerate(indegrees):
 		if v == 0:
 			q.append(i)
 	while q:
 		count += 1
-		course = q.pop()
+		course = q.pop(0)
 		for i in range(num_courses):
 			if matrix[course][i]:
-				indegrees[course][i] -= 1
-				if indegrees == 0:
+				indegrees[i] -= 1
+				if indegrees[i] == 0:
 					q.append(i)
 	return count == num_courses
+
+
+print can_finish(2, [[1, 0]]) == True
