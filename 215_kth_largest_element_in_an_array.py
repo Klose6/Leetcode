0869401205f5cef1eb74e48ae88a99ddf741186b
Klose6 <scsehebut@gmail.com>
2018-08-16 23:31:30 -0700

    Aug16:1

--- 215_kth_largest_element_in_an_array.py
+++ 215_kth_largest_element_in_an_array.py
@@ -1,16 +1,27 @@
 """
 215 kth largest element in an array
 """
+import random
 from heapq import heappush, heappop
 
 
 def find_kth_largest(nums, k):
 	pq = []
+	# print nums
+	# randomize(nums)
+	# print nums
+	# random.shuffle(nums)
 	for i, v in enumerate(nums):
 		heappush(pq, v)
 		if i >= k:
 			heappop(pq)
 	return heappop(pq)
+
+
+def randomize(a):
+	for i in range(len(a)):
+		pos = random.randint(0, i)
+		a[i], a[pos] = a[pos], a[i]
 
 
 print find_kth_largest([3, 2, 1, 5, 6, 4], 2) == 5
