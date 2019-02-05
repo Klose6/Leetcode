d345dfd6dc6ee49114ee92a504ea35ad770b609c
Klose6 <scsehebut@gmail.com>
2019-02-04 22:28:02 -0800

    02/04/2019

--- 162_find_peak_elements.py
+++ 162_find_peak_elements.py
@@ -11,12 +11,27 @@ class Solution(object):
 			mid = lo+(hi-lo)/2
 			mid2 = mid+1
 			if nums[mid] < nums[mid2]:
-				lo=mid2
+        lo = mid2
 			else:
-				hi=mid
+        hi = mid
 		return nums[lo]
+
+  def find_peak_elements1(self, nums):
+    if not nums:
+      return
+    n = len(nums)
+    for i in range(1, n):
+      if nums[i] < nums[i - 1]:
+        return nums[i - 1]
+    return nums[n - 1]
+
 s=Solution()
-print s.find_peak_elements([1])
-print s.find_peak_elements([1,2])
-print s.find_peak_elements([2,1])
-print s.find_peak_elements([1,3,2])
+assert s.find_peak_elements([1]) == 1
+assert s.find_peak_elements([1, 2]) == 2
+assert s.find_peak_elements([2, 1]) == 2
+assert s.find_peak_elements([1, 3, 2]) == 3
+
+assert s.find_peak_elements1([1]) == 1
+assert s.find_peak_elements1([1, 2]) == 2
+assert s.find_peak_elements1([2, 1]) == 2
+assert s.find_peak_elements1([1, 3, 2]) == 3
