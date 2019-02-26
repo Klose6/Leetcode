4e565e5f09933d1be44e172ef6a49e922e067001
Klose6 <scsehebut@gmail.com>
2019-02-25 22:35:46 -0800

    594

--- 594_longest_harmonious_sub.py
+++ 594_longest_harmonious_sub.py
@@ -1,18 +1,18 @@
 """
-Longest harmonious subsequence
+Longest harmonious sub-sequences
 """
 import collections
 
 class Solution:
   def findLHS(self, nums):
-
     count = collections.Counter(nums)
-    # for i in nums:
-    #   count[i] += 1
-    print(count)
-    count = [count[i]+count[i+1] for i in count if count[i+1]] or [0]
-    print(count)
-    return max(count)
+    res = 0
+    for i in count:
+      if i + 1 in count:
+        res = max(res, count[i] + count[i + 1])
+    return res
+    # count = [count[i]+count[i+1] for i in count if count[i+1]] or [0]
+    # return max(count)
 
 print(Solution().findLHS([1,3,2,2,5,2,3,7]))
 print(Solution().findLHS([1,1,1]))
