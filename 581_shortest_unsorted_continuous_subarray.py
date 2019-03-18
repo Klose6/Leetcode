bb1ee4b49ef82760bca6bb520a2cfaa6185c13fc
Klose6 <scsehebut@gmail.com>
2019-03-17 19:33:21 -0700

    581

--- 581_shortest_unsorted_continuous_subarray.py
+++ 581_shortest_unsorted_continuous_subarray.py
@@ -1,19 +1,19 @@
 """
-581 shortest unsroted continous subarray
+581 shortest unsorted continous subarray
 """
 import sys
 
 
 def findUnsortedSubarray(nums):
   begin, end = -1, -2
-  min, max = sys.maxsize, -sys.maxsize - 1  # float("inf"), float("-inf")
+  min_val, max_val = float("inf"), float("-inf")  # sys.maxsize, -sys.maxsize - 1  #
   n = len(nums)
   for i in range(n):
-    if nums[i] > max: max = nums[i]
-    if nums[n - 1 - i] < min: min = nums[n - 1 - i]
-    if nums[i] < max:
+    max_val = max(max_val, nums[i])
+    min_val = min(min_val, nums[n - 1 - i])
+    if nums[i] < max_val:
       end = i
-    if nums[n - 1 - i] > min:
+    if nums[n - 1 - i] > min_val:
       begin = n - 1 - i
   return end - begin + 1
 
