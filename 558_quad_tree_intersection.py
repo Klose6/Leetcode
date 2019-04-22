f75512f4a3a67ac9ec8a5094bffbf96d7114638e
Klose6 <scsehebut@gmail.com>
2019-04-22 15:03:25 -0700

    558

---
+++ 558_quad_tree_intersection.py
@@ -0,0 +1,30 @@
+"""
+558 quad tree intersection
+"""
+class Node:
+  def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
+    self.val = val
+    self.isLeaf = isLeaf
+    self.topLeft = topLeft
+    self.topRight = topRight
+    self.bottomLeft = bottomLeft
+    self.bottomRight = bottomRight
+
+def quad_tree_intersection(q1, q2):
+  if q1.isLeaf: #if q1 is leaf and it value is true, then we can directly return q1, otherwise we rely on q2
+    return q1.val if q1 else q2
+  elif q2.isLeaf:
+    return q2.val if q2 else q1
+  else:
+    tLeft = quad_tree_intersection(q1.topLeft, q2.topLeft)
+    tRight = quad_tree_intersection(q1.topRight, q2.topRight)
+    bLeft = quad_tree_intersection(q1.bottomLeft, q2.bottomLeft)
+    bRight = quad_tree_intersection(q1.bottomRight, q2.bottomRight)
+    if tLeft.isLeaf and tRight.isLeaf and bLeft.isLeaf and bRight.isLeaf and \
+        tLeft.val == tRight.val == bLeft.val == bRight.val:
+      # if all the nodes are leaves and they have the same value
+      node = Node(tLeft.val, True, None, None, None, None)
+    else:
+      node = Node(False, False, tLeft, tRight, bLeft, bRight)
+  return node
+
