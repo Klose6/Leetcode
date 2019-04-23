c4c404960286ab8f235b68b46381e1ea4b7596b4
Klose6 <scsehebut@gmail.com>
2019-04-23 14:30:23 -0700

    556

--- 556_next_greater_element_III.py
+++ 556_next_greater_element_III.py
@@ -3,5 +3,28 @@
 """
 
 def next_greater_element(num):
-  num = str(num)
-  for i in
+  s = list(str(num))
+  # print(f"s: {s}")
+  i = len(s) - 1
+  # start from the right most element and find the first element that is smaller than the digit next to it
+  while i > 0 and s[i] <= s[i-1]:
+      i -= 1
+  # print(f"i: {i}")
+  # if no such element, which means the nums are sorted reversely, then return -1
+  if i == 0: return -1
+  # find the smallest digit on right side of i-1 that is greater than i-1
+  x, smallest = s[i-1], i
+  for j in range(i+1, len(s)):
+    if s[j] > s[i-1] and s[j] <= s[smallest]:
+      smallest = j
+  # swap the sbove found smallest digit with i-1
+  s[i-1], s[smallest] = s[smallest], s[i-1]
+  # sort the digits after i-1 in ascending order
+  s[i:] = sorted(s[i:])
+  s = "".join(s)
+  # print(f"s: {s}")
+  return int(s) if int(s) <= (1<<31-1) else -1
+
+print(next_greater_element(12)) # 21
+print(next_greater_element(21)) # -1
+print(next_greater_element(4521)) # -1
