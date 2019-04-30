b907067f39937114826a2697f0eecba326bd7f08
Klose6 <scsehebut@gmail.com>
2019-04-30 15:39:52 -0700

    517

--- 517_super_washing_machine.py
+++ 517_super_washing_machine.py
@@ -8,12 +8,12 @@ class Solution(object):
 		n = len(machines)
 		if sum(machines) % n != 0: return -1
 		count, res = 0, 0
-		avg = sum(machines) / n
+		avg = sum(machines) // n
 		for load in machines:
-			count += load - avg
-			res = max(res, max(load - avg, abs(count)))
+			count += load - avg # load- avg is the gain/lose
+			res = max(res, max(load - avg, abs(count))) # the least steps is determined by the peak of abs(count) and gain/lose
 		return res
 
 
-print Solution().super_washing_machine([1, 0, 5]) == 3
-print Solution().super_washing_machine([0, 3, 0]) == 2
+print(Solution().super_washing_machine([1, 0, 5])) # 3
+print(Solution().super_washing_machine([0, 3, 0])) # 2
