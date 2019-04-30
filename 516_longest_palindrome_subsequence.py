0608736a6604834cae47c153fffb7bc2f9220fd7
Klose6 <scsehebut@gmail.com>
2019-04-30 15:58:26 -0700

    516

---
+++ 516_longest_palindrome_subsequence.py
@@ -0,0 +1,19 @@
+"""
+516 longest palindrome subsequence
+"""
+
+def findLongestPalindrome(s):
+  if not s: return 0
+  n = len(s)
+  dp = [[0]*n for _ in range(n)]
+  for i in range(n)[::-1]:
+    dp[i][i] = 1
+    for j in range(i+1, n):
+      if s[i] == s[j]:
+        dp[i][j] = dp[i+1][j-1] + 2
+      else:
+        dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+  return dp[0][n-1]
+
+print(findLongestPalindrome("bbbab")) # 4
+print(findLongestPalindrome("cbbd")) # 2
