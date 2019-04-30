9b676104077f34d2716d551cb1f6f9dc1d7a329b
Klose6 <scsehebut@gmail.com>
2019-04-29 22:44:46 -0700

    518

--- 518_coin_change_2.py
+++ 518_coin_change_2.py
@@ -6,9 +6,9 @@ def change(amount, coins):
   dp = [0] * (amount+1)
   dp[0] = 1 # initialize the array
   for c in coins:
-    for i in range(1, amount+1):
-      if i >= c: # for each single coin amount c, all the index divisible by c will be assigned 1
-        dp[i] += dp[i-c]
+    for i in range(c, amount+1):
+      # for each single coin amount c, all the index divisible by c will be assigned 1
+      dp[i] += dp[i-c]
   return dp[amount]
 
 print(change(5, [1,2,5])) #4
