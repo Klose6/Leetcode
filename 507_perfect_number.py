c66a7da362ab5f52daab186dfef1e4ec1aa521c9
Klose6 <scsehebut@gmail.com>
2019-05-01 14:28:40 -0700

    507

--- 507_perfect_number.py
+++ 507_perfect_number.py
@@ -5,17 +5,15 @@ https://leetcode.com/problems/perfect-number/#/solution
 from math import sqrt
 
 def perfect(num):
-  if num == 1:
-    return False
+  if num <= 1: return False
   res = 1
   for i in range(2, int(sqrt(num)+1)):
-    if num % i == 0:
+    if num % i == 0: # a number that is a square of another num will not be a perfect number
       res += i + num / i
   return res == num
 
 def perfect_2(num):
-  if num == 1:
-    return False
+  if num <= 1:return False
   return sum([i for i in range(1, num) if num%i == 0]) == num
 
 n = 28
