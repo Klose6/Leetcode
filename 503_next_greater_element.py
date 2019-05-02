ce32c8fb767b8508bf225eac2ef8e6ca13578b84
Klose6 <scsehebut@gmail.com>
2019-05-02 11:55:58 -0700

    503

--- 503_next_greater_element.py
+++ 503_next_greater_element.py
@@ -6,8 +6,8 @@ https://leetcode.com/problems/next-greater-element-ii/#/discuss
 def nextGreaterElements(nums):
   stack, res = [], [-1] * len(nums)
   for i in list(range(len(nums)))*2:
-    while stack and nums[stack[-1]] < nums[i]:
-      res[stack.pop()] = i
+    while stack and nums[stack[-1]] < nums[i]: # find all the bigger number for all the elements
+      res[stack.pop()] = i # may update the same item in res twice, but that is fine
     stack.append(i)
   return res
 
