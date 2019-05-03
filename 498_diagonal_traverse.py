4b01daf83b09750172099d2f562e028799d914e2
Klose6 <scsehebut@gmail.com>
2019-05-02 22:46:06 -0700

    498

--- 498_diagonal_traverse.py
+++ 498_diagonal_traverse.py
@@ -2,28 +2,30 @@
 def diagonal_traverse(matrix):
   if matrix is None:
     return
-  m = len(matrix)
-  n = len(matrix[0])
-  dirs = ((-1, 1), (1, -1))
-  col, row, d = 0, 0, 0
-  for i in range(m*n):
-    print(matrix[row][col])
+  m, n = len(matrix), len(matrix[0])
+  dirs = ((-1, 1), (1, -1)) # there is only 2 directions
+  col, row, d = 0, 0, 0 # the start point and direction
+  res = []
+  for i in range(m*n): # for all the elements
+    res.append(matrix[row][col])
     row += dirs[d][0]
     col += dirs[d][1]
-    if row >= m:
+    # very time meet a boundary, need to change the direction
+    if row >= m: # for the bottom boundary
       row -= 1
       col += 2
       d = 1-d
-    if col >= n:
+    if col >= n: # for the right boundary
       row += 2
       col -= 1
       d = 1-d
-    if row < 0:
+    if row < 0: # for the top boundary
       row = 0
       d = 1-d
-    if col < 0:
+    if col < 0: # for the left boundary
       col = 0
       d = 1-d
+  return res
 
 #https://discuss.leetcode.com/topic/77933/sorting-and-normal-python
 def diagonalTraverse2(matrix):
@@ -36,7 +38,7 @@ def diagonalTraverse2(matrix):
 def diagonalTraverse3(matrix):
   if matrix is None:
     return
-  m,n = len(matrix), len( matrix[0])
+  m,n = len(matrix), len(matrix[0])
   return [matrix[i][d-i]
             for d in range(m+n-1)
             for i in range(max(0, d-n+1), min(m, d+1))[::d%2*2-1]]
