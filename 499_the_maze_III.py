77ca048931a9b110503e1fad8730cb9757972618
Klose6 <scsehebut@gmail.com>
2019-05-02 18:27:07 -0700

    499

--- 499_the_maze_III.py
+++ 499_the_maze_III.py
@@ -5,7 +5,7 @@ https://discuss.leetcode.com/category/648/the-maze-iii
 http://www.cnblogs.com/grandyang/p/6746528.html
 https://discuss.leetcode.com/topic/77076/simple-python-explanation
 """
-from queue import heappush, heappop
+from heapq import heappush, heappop
 
 def findPath(start, hole, board):
   if not start or not hole or not board:
@@ -15,8 +15,8 @@ def findPath(start, hole, board):
   def go(c, i, j):
     x,y = c
     d = 0
-    while 0<=x+i<m and 0<=y+j<n and board[x+i][y+j] == "0":
-      if (x,y) == hole:
+    while 0 <= x+i < m and 0 <= y+j < n and board[x+i][y+j] == "0":
+      if (x, y) == hole:
         break
       x, y = x+i, y+j
       d += 1
@@ -24,6 +24,8 @@ def findPath(start, hole, board):
   q = list()
   visited = set()
   heappush(q, (0, "", start))
+  # using the (distance, path, points) in the heap,
+  # which guarantees to find the lexicographically smaller path first
   while q:
     count, path, cur = heappop(q)
     # print(count, path, cur)
@@ -32,9 +34,8 @@ def findPath(start, hole, board):
     visited.add(cur)
     if cur == hole:
       return path
-    for d,i,j in [("u",-1,0),("l",0,-1),("r",0,1),("d",1,0)]:
-      l, point = go(cur, i,j)
-      # if point != cur:
+    for d, i, j in [("u", -1, 0), ("l", 0, -1), ("r", 0, 1), ("d", 1, 0)]:
+      l, point = go(cur, i, j)
       heappush(q, (l+count, path+d, point))
   return "impossible"
 
