a12277a3fbb65e893f407e865fc39bf06c2c36df
Klose6 <scsehebut@gmail.com>
2019-05-02 17:49:39 -0700

    501

--- 501_find_mode_in_bst.py
+++ 501_find_mode_in_bst.py
@@ -2,50 +2,34 @@
 find mode in binary search tree 501
 https://leetcode.com/problems/find-mode-in-binary-search-tree/#/solutions
 """
-class node:
-  def __init__(self, val):
-    self.val = val
-    self.left = None
-    self.right = None
 
-def find_mode_bst(root):
-  inorder(root)
-  global res
-  res = list()
-  inorder(root)
-  return res
+from utils import TreeNode
 
-def inorder(root):
-  if not root:
-    return
-  inorder(root.left)
-  handle_val(root.val)
-  inorder(root.right)
+class Solution:
+  def findMode(self, root):
+    self.mode_val = None
+    self.mode_count = 0
+    self.cur_val = None
+    self.cur_count = 0
+    self.res = []
+    self.inorder(root)
+    return self.res
 
-cur_val = None
-cur_count = 0
-mode_count = 0
-max_count = 0
-cur_mode = None
-res = None
+  def inorder(self, node):
+    if not node: return
+    self.inorder(node.left)
+    self.handle(node.val)
+    self.inorder(node.right)
 
-def handle_val(val):
-  global cur_val, max_count, cur_mode, mode_count, res
-  if val != cur_val:
-    cur_val = val
-    cur_count = 0
-  cur_count += 1
-  if cur_count > max_count:
-    max_count = cur_count
-    cur_mode = val
-  elif cur_count == max_count:
-    if res != None:
-      res.append(val)
-    mode_count += 1
-
-root = node(2)
-root.left = node(1)
-root.right = node(3)
-
-find_mode_bst(root)
-print(res)
+  def handle(self, val):
+    if val != self.cur_val:
+      self.cur_val = val
+      self.cur_count = 0
+    else:
+      self.cur_count += 1
+    if self.cur_count > self.mode_count:
+      self.mode_count = self.cur_count
+      self.mode_val = self.cur_val
+      self.res = [self.cur_val]
+    elif self.cur_count == self.mode_count:
+      self.res.append(self.cur_val)
