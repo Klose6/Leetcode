b9b126c3c034705a95458ebc94dd84292b94dc47
Klose6 <scsehebut@gmail.com>
2019-05-04 23:12:59 -0700

    490

---
+++ 490_the_maze.py
@@ -0,0 +1,31 @@
+"""
+490 the maze
+"""
+from queue import Queue
+
+def hasPath(matrix, start, dest):
+  if not matrix: raise ValueError("Wrong input")
+  m, n = len(matrix), len(matrix[0])
+  q = Queue()
+  q.put(start)
+  visited = {tuple(start)} # list is not hashable
+  while not q.empty():
+    cur = q.get()
+    # print(f"cur: {cur}")
+    if cur == tuple(dest): return True
+    for d in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+      x = cur[0] + d[0]
+      y = cur[1] + d[1]
+      while 0<=x<m and 0<=y<n and matrix[x][y] == "0": # keep going until meet a wall
+        x += d[0]
+        y += d[1]
+      x -= d[0] # roll back the valid position
+      y -= d[1]
+      if (x, y) not in visited:
+        visited.add((x, y))
+        q.put((x, y))
+  return False
+
+matrix = [list("00100"), list("00000"), list("00010"), list("11011"), list("00000")]
+print(hasPath(matrix, [0,4], [4,4])) # True
+print(hasPath(matrix, [0,4], [3,2])) # False
