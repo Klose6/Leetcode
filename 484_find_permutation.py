4b3f48d54615fefa3a24461603422eed8c6fc939
Klose6 <scsehebut@gmail.com>
2019-05-07 23:12:54 -0700

    484

--- 484_find_permutation.py
+++ 484_find_permutation.py
@@ -3,4 +3,21 @@
 """
 
 def findPermutation(s):
-  n = len(s) + 1
+  n = len(s)
+  nums = [i+1 for i in range(n+1)]
+  i = 0
+  while i < n:
+    if s[i] != "D":
+      i += 1
+    else: # find the continuous Ds and reverse them
+      j = i+1
+      while j < n and s[j] == "D":
+        j += 1
+      # print(f"{i}, {j}")
+      nums[i:j+1] = nums[i:j+1][::-1]
+      i = j
+  return nums
+
+print(findPermutation("D")) # 21
+print(findPermutation("ID")) # 213
+print(findPermutation("DDIIDI")) # 3214657
