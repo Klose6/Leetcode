9d03eb859b131602bbabb7c933d4a02e8101cc98
Klose6 <scsehebut@gmail.com>
2019-05-12 13:48:23 -0700

    480

--- 480_sliding_window_median.py
+++ 480_sliding_window_median.py
@@ -25,10 +25,39 @@ class Solution(object):
 			return
 		lh, rh = [], []
 		res = []
-		for i in range(k):
-			heappush(lh, i)
-		for i in range(k):
+		for i, v in enumerate(nums[:k]):
+			heappush(lh, (-v, i))
+		for i in range(k - k//2):
+			val, idx = heappop(lh)
+			heappush(rh, (-val, idx))
+		for i, v in enumerate(nums[k:], k):
+			print(f"lh: {lh}, rh: {rh}")
+			res.append(rh[0][0]/1.0 if k%2 else (rh[0][0]-lh[0][0])/2.0)
+			# remove the (i-k) element
+			if v >= rh[0][0]:
+				heappush(rh, (v, i))
+				if nums[i-k] <= rh[0][0]:
+					val, idx = heappop(rh)
+					heappush(lh, (-val, idx))
+			else:
+				heappush(lh, (-v, i))
+				if nums[i-k] >= rh[0][0]:
+					val, idx = heappop(lh)
+					heappush(rh, (-val, idx))
+			# adjust the heap
+			print(f"before adjust: {lh}, {rh}")
+			# no need to delete the i-k element immediately for each iteration except it's on the top of the heaps
+			while lh and lh[0][1] <= i-k: heappop(lh)
+			while rh and rh[0][1] <= i-k: heappop(rh)
+		res.append(rh[0][0] / 1.0 if k % 2 else (rh[0][0] - lh[0][0]) / 2.0)
+		return res
 
 
 s=Solution()
-print(s.sliding_window_medians([1,3,-1,-3], 3)) #[1,-1]
+# print(s.sliding_window_medians([1,3,-1,-3], 3)) #[1,-1]
+# print(s.sliding_window_medians2([1,3,-1,-3], 3)) #[1, -1]
+
+# print(s.sliding_window_medians2([10,2], 1)) # [1,2]
+print(s.sliding_window_medians2([9,7,0,3,9,8,6,5,7,6], 2))
+
+# print(s.sliding_window_medians2([1,3,-1,-3,5,3,6,7], 3)) # [1.0,-1.0,-1.0,3.0,5.0,6.0]
