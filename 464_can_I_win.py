d255ebf3f3799d7f519c8d4b6423ae46b356a5db
Klose6 <scsehebut@gmail.com>
2019-05-26 22:36:07 -0700

    464

---
+++ 464_can_I_win.py
@@ -0,0 +1,31 @@
+"""
+464 can I win
+"""
+
+def canWin(maxChoosableInteger, desiredTotal):
+  sum = maxChoosableInteger * (maxChoosableInteger + 1) // 2
+  if sum < desiredTotal:
+    return False
+  if desiredTotal <= 0:
+    return True
+  used = [0] * (maxChoosableInteger + 1)
+  return dfs(used, desiredTotal)
+
+cache = {}
+
+def dfs(used, desiredTotal):
+  if desiredTotal <= 0: return False
+  k = int("".join(map(str, used)))
+  if k in cache: return cache[k]
+  for i, v in enumerate(used):
+    if v == 0:
+      used[i] = 1
+      if not dfs(used, desiredTotal - i):
+        cache[int("".join(map(str, used)))] = True
+        used[i] = 0
+        return True
+      used[i] = 0
+  cache[k] = False
+  return False
+
+print(canWin(10, 11)) # False
