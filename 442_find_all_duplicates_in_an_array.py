6997e28ff47ba0fafd6ec436b927a3e4f8b6cdcf
Klose6 <scsehebut@gmail.com>
2019-06-10 22:37:47 -0700

    442

---
+++ 442_find_all_duplicates_in_an_array.py
@@ -0,0 +1,16 @@
+"""
+442 find all duplicates in an array
+"""
+
+def findAllDuplicates(nums):
+  """
+  for [i] mark the [[i]-1] to negative, then wehen we meet the negative values again, we know it is duplicates
+  """
+  if not nums: return []
+  res = []
+  for i, v in enumerate(nums):
+    if nums[abs(v) - 1] < 0:
+      res.append(abs(v))
+    else:
+      nums[abs(v) - 1] = -nums[abs(v) - 1]
+  return res
