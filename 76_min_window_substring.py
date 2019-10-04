c3bb201738de5dd5ddd5a19a50ef703787fc822c
Klose6 <scsehebut@gmail.com>
2019-10-03 21:45:00 -0700

    Add new solution

--- 76_min_window_substring.py
+++ 76_min_window_substring.py
@@ -2,8 +2,29 @@
 minimum window substring
 """
 
-from collections import Counter
+from collections import Counter, defaultdict
 
+
+def minWindow(s: str, t: str) -> str:
+	if not s or not t or len(s) < len(t): return ""
+	required = Counter(t)
+	requiredCounter = len(required)
+	formed = 0
+	window = defaultdict(int)
+	res = (float("inf"), 0, 0)
+	l = 0
+	for i, c in enumerate(s):
+		window[c] += 1
+		if window[c] == required[c]:
+			formed += 1
+		while l <= i and formed == requiredCounter:
+			if i - l + 1 < res[0]:
+				res = (i - l + 1, l, i)
+			window[s[l]] -= 1
+			if s[l] in required and window[s[l]] < required[s[l]]:
+				formed -= 1
+			l += 1
+	return "" if res[0] == float("inf") else s[res[1]: res[2] + 1]
 
 def min_window_substring(s, t):
 	if not s or len(s) < len(t):
@@ -22,4 +43,6 @@ def min_window_substring(s, t):
 	return s[start:end]
 
 
-print min_window_substring("ADOBECODEBANC", "ABC")  # BANC
+print(min_window_substring("ADOBECODEBANC", "ABC"))  # BANC
+print(min_window_substring("a", "b")) # ""
+print(min_window_substring("a", "a")) # a"
