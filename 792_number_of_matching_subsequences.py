338da8147461ff445067818ea8fa42fa76ebf23f
Klose6 <scsehebut@gmail.com>
2021-06-24 11:43:53 -0700

    792

--- 792_number_of_matching_subsequences.py
+++ 792_number_of_matching_subsequences.py
@@ -2,11 +2,13 @@ import collections
 
 
 def numMatchingSubseq(S, words):
+  # Time complexity: O(S + #words)
+  # Space complexity: O(#words)
   waiting = collections.defaultdict(list)
-  for w in words:
+  for w in words:  # O(#words)
     waiting[w[0]].append(iter(w[1:]))
-  for s in S:
-    for c in waiting.pop(s, ()):
+  for s in S:  # O(S)
+    for c in waiting.pop(s, ()):  # each word will only be popped and added to the dict once for each loop
       waiting[next(c, None)].append(c)
   return len(waiting[None])
 
